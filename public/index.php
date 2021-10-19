e3b055605e7aff5d99405e4722de3528b62966c0
Gwilherm Couffy <90566016+Hellnettles@users.noreply.github.com>
2021-10-19 09:38:56 +0200

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require '../src/Wcs/Hello.php';
+
+$bonjour = new Hello();
+$bonjour->talk();
---
+++ src/Wcs/Hello.php
@@ -0,0 +1,9 @@
+<?php
+
+use Hello;
+
+class Hello {
+    public function talk() {
+        echo "Hello World !";
+    }
+}
